400bccb2088ea37882573ac4b2900c50ff73512d
maza <markelov.69@bk.ru>
2018-12-29 14:57:53 +0400

    minor styling canges on 'Github' page

--- src/pages/github/components/tab/components/content/index.jsx
+++ src/pages/github/components/tab/components/content/index.jsx
@@ -10,18 +10,26 @@ export default class Content extends React.Component {
         const { tab } = this.props;
         const { data, isDataFetching } = tab.tabData;
 
-        if (isDataFetching) return <div>Fetching data from Github...</div>;
+        if ((tab.isTabVirgin || data.length === 0) && isDataFetching) return <div>Fetching data from Github...</div>;
         if (tab.isTabVirgin) return <div>Apply some filters to get results</div>;
         if (data.length === 0) return <div>Nothing found</div>;
 
-        return data.map(item => {
-            let text = item.login || item.full_name;
+        return (
+          <div styleName="items-list">
+              { isDataFetching && <div styleName="fetching-overlap" /> }
 
-            return (
-                <div styleName="item" key={item.id}>
-                    <a href={item.html_url} target="_blank">{text}</a>
-                </div>
-            )
-        });
+              {
+                  data.map(item => {
+                      let text = item.login || item.full_name;
+
+                      return (
+                          <div styleName="item" key={item.id}>
+                              <a href={item.html_url} target="_blank">{text}</a>
+                          </div>
+                      )
+                  })
+              }
+          </div>
+        );
     }
 }
