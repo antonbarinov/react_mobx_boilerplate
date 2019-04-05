f5a390b320e073f5af187d0bfc6562ae3df37645
Anton Barinov <anton@barinov.com>
2019-04-05 11:56:38 +0400

    refactoring

--- src/helpers/misc.js
+++ src/helpers/misc.js
@@ -5,3 +5,8 @@ export function sleep(ms = 0) {
         }, ms);
     });
 }
+
+export function validateEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
