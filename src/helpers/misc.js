2e61ad5557f76f1eb95c72d90b38cad5a818636a
Anton Barinov <anton@barinov.com>
2019-04-05 15:51:01 +0400

    refactoring

--- src/helpers/misc.js
+++ src/helpers/misc.js
@@ -10,3 +10,13 @@ export function validateEmail(email) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
+
+export function arrayChucnk(array = [], chunkSize = 2) {
+    let result = [];
+    let i, j;
+    for (i = 0, j = array.length; i < j; i += chunkSize) {
+        result.push(array.slice(i, i + chunkSize));
+    }
+
+    return result;
+}
