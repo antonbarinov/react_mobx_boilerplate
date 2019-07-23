05a9c9953058e29a27f08a5ddf44e4d5b621264e
Anton Barinov <anton@barinov.com>
2019-07-23 16:46:53 +0400

    refactoring

--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,6 @@ import '@babel/polyfill'; // Need for async/await
 
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import * as mobx from 'mobx';
-
-mobx.configure({
-    reactionScheduler: (f) => {
-        setTimeout(f, 0);
-    }
-});
 
 import App from './App';
 
