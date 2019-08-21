6cb65d2259438c4b77299f836a1b45481e3155fd
Anton Barinov <anton@barinov.com>
2019-08-21 23:53:33 +0400

    refactoring

--- src/hooks/layouts/hookHandleInitialFetching.js
+++ src/hooks/layouts/hookHandleInitialFetching.js
@@ -3,24 +3,24 @@ import userState from 'globalState/user';
 
 let timeout = null;
 
+function handleInitialFetching() {
+    clearTimeout(timeout);
+
+    if (!userState.initialFetching) {
+        // Let's react take some time to render page before we hide preloader
+        timeout = setTimeout(() => {
+            // Hide global preloader
+            document.getElementById('preloader').classList.add('hidden');
+        }, 10);
+    }
+    else {
+        // Show global preloader
+        document.getElementById('preloader').classList.remove('hidden');
+    }
+}
+
 // This effect fires only when component will unmount
 export function hookHandleInitialFetching() {
-    function handleInitialFetching() {
-        clearTimeout(timeout);
-
-        if (!userState.initialFetching) {
-            // Let's react take some time to render page before we hide preloader
-            timeout = setTimeout(() => {
-                // Hide global preloader
-                document.getElementById('preloader').classList.add('hidden');
-            }, 10);
-        }
-        else {
-            // Show global preloader
-            document.getElementById('preloader').classList.remove('hidden');
-        }
-    }
-
     const dispose = reaction(
         () => {
             return userState.initialFetching;
