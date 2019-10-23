17988852d71d85a8217ef3508aa5b43dfb12ff79
maza <markelov.69@bk.ru>
2019-10-23 21:33:05 +0400

    refactoring

--- src/helpers/misc.js
+++ src/helpers/misc.js
@@ -45,7 +45,22 @@ function isPrimitive(obj) {
 export function deepEqual(obj1, obj2) {
     if (obj1 === obj2) return true;
     if (isPrimitive(obj1) && isPrimitive(obj2)) return obj1 === obj2;
-    if (Object.keys(obj1).length !== Object.keys(obj2).length) return false;
+    let obj1Len;
+    let obj2Len;
+
+    if (Array.isArray(obj1)) {
+        obj1Len = obj1.length;
+    } else {
+        obj1Len = Object.keys(obj1).length;
+    }
+
+    if (Array.isArray(obj2)) {
+        obj2Len = obj2.length;
+    } else {
+        obj2Len = Object.keys(obj2).length;
+    }
+
+    if (obj1Len !== obj2Len) return false;
 
     for (let key in obj1) {
         if (!(key in obj2)) return false; //other object doesn't have this prop
