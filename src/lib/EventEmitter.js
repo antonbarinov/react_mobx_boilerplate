7edf4c3272d5321aab60cfa8b53aecee9f99d344
Anton Barinov <anton@barinov.ru>
2019-12-29 22:40:55 +0400

    refactoring

---
+++ src/lib/EventEmitter.js
@@ -0,0 +1,53 @@
+export class EventEmitter {
+    constructor() {
+        this.events = {};
+    }
+
+    // eventName can be also array of event names
+    emit(eventName, ...restParams) {
+        let eventNames = eventName;
+        if (!Array.isArray(eventName)) eventNames = [eventName];
+
+        for (const eventName of eventNames) {
+            const events = this.events[eventName];
+            if (events) {
+                events.forEach((fn) => {
+                    fn.call(null, ...restParams);
+                });
+            }
+        }
+    }
+
+    // eventName can be also array of event names
+    subscribe(eventName, fn) {
+        let eventNames = eventName;
+        if (!Array.isArray(eventName)) eventNames = [eventName];
+
+        for (const eventName of eventNames) {
+            if (!this.events[eventName]) {
+                this.events[eventName] = new Set();
+            }
+
+            const events = this.events[eventName];
+
+            events.add(fn);
+        }
+
+        // or use unsubscribe function
+        return this.unsubscribe.bind(this, eventName, fn);
+    }
+
+    // eventName can be also array of event names
+    unsubscribe(eventName, fn) {
+        let eventNames = eventName;
+        if (!Array.isArray(eventName)) eventNames = [eventName];
+
+        for (const eventName of eventNames) {
+            const events = this.events[eventName];
+
+            if (events) events.delete(fn);
+        }
+    }
+}
+
+export const globalEventEmitter = new EventEmitter();
