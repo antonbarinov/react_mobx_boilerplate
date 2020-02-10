ba6aeeb0a1780362a51dc61af0a5fd80881b9a06
Anton Barinov <anton@barinov.com>
2020-02-10 18:26:29 +0400

    refactoring

--- src/lib/router.jsx
+++ src/lib/router.jsx
@@ -124,8 +124,12 @@ export function Router({ routes, global, hashMode }) {
                 if (route === '' || route === '*') continue;
                 const component = routes[route];
 
+                let routePath = route;
+                if (routePath.substr(-1) === '/')
+                    routePath = routePath.substr(0, routePath.length - 1);
+
                 const keys = [];
-                const regexp = pathToRegexp(route, keys);
+                const regexp = pathToRegexp(routePath, keys);
                 const res = exec(regexp, path, keys);
 
                 if (res) {
